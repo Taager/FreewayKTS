e608657dfff16ffc4dc164c8afafe56b3f18793b
Taager <taager2201@gmail.com>
2019-08-05 13:08:55 +0200

    Basics added

---
+++ Main.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class FreewayKanbanizeToScrumwise
+{
+	public void Main()
+	{
+        var kanbanizeApiKey = Login("", "");
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+    #region kanbanize
+    static string Login(string username, string password)
+    {
+        RestClient client = new RestClient("https://freeway.kanbanize.com/index.php/api/kanbanize/login/");
+        RestRequest request = new RestRequest(Method.POST);
+        request.RequestFormat = DataFormat.Json;
+        request.AddBody();
+        request.AddParameter("email", "taager2201@gmail.com");
+        request.AddParameter("pass", "9uYOY6SLe7gZHXlPBXxgVYr4ps0HyqgSP64JAOiR");
+
+        var result = client.Execute<LoginResult>(request);
+        //client.
+        return result.Data.ApiKey;
+    }
+    
+    #endregion
+}
